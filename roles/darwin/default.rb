b531857b735fce169dd66bca5c66cbfc079980e5
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-03-28 21:10:52 +0900

    fix for test

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -3,3 +3,7 @@ execute 'Install Homebrew' do
   command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   not_if 'test $(which /usr/local/bin/brew)'
 end
+
+file File.join(ENV["HOME"], "hello.txt") do
+    content "This is darwin roles."
+end
