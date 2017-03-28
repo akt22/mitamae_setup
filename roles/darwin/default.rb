548372a0131cdd74262583d3af89c161a5f6a0ac
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-03-28 21:29:14 +0900

    fix

--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -1,8 +1,8 @@
-# execute 'Install Homebrew' do
-#   command 'sudo xcodebuild -license && xcode-select --install'
-#   command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
-#   not_if 'test $(which /usr/local/bin/brew)'
-# end
+execute 'Install Homebrew' do
+  command 'sudo xcodebuild -license && xcode-select --install'
+  command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  not_if 'test $(which /usr/local/bin/brew)'
+end
 
 file File.join(ENV["HOME"], "hello.txt") do
     content "This is darwin roles."
