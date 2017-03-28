201c0bdb59b25ca64ab6893e0e465099558d7ca6
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-03-28 22:13:55 +0900

    test clone dotfiles

--- roles/base/default.rb
+++ roles/base/default.rb
@@ -1,7 +1,13 @@
-node[:packages][:platform].each do |p|
-    package p
+node[:packages][node[:platform]].each do |p|
+  package p
 end
 
 node[:packages].each do |p|
-    package p
+  package p
 end
+
+git "clone dotfiles" do
+  destination File.join(ENV["HOME"], 'dotfiles')
+  repository "https://github.com/akt22/dotfiles.git"
+end
+
