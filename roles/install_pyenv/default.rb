936dc2cdde87d0830014ab4d05985bed8bc2cb3c
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-03-31 01:58:48 +0900

    insert pyenv

--- roles/install_pyenv/default.rb
+++ roles/install_pyenv/default.rb
@@ -5,22 +5,22 @@ git "clone pyenv" do
   repository "https://github.com/pyenv/pyenv.git"
 end
 
-# pyenv_init = <<-EOS
-#   export PYENV_ROOT=#{PYENV_ROOT}
-#   export PATH="#{PYENV_ROOT}/bin:${PATH}"
-#   eval "$(pyenv init --no-rehash -)"
-# EOS
+pyenv_init = <<-EOS
+  export PYENV_ROOT=#{PYENV_ROOT}
+  export PATH="#{PYENV_ROOT}/bin:${PATH}"
+  eval "$(pyenv init --no-rehash -)"
+EOS
 
-# execute "install python" do
-#   command "#{pyenv_init} pyenv install #{node[:pyenv][:version]}"
-#   not_if "#{pyenv_init} pyenv version | grep #{node[:pyenv][:version]}"
-# end
+execute "install python" do
+  command "#{pyenv_init} pyenv install #{node[:pyenv][:version]}"
+  not_if "#{pyenv_init} pyenv version | grep #{node[:pyenv][:version]}"
+end
 
-# execute "set global python 3.5.2" do
-#   command "#{pyenv_init} pyenv global #{node[:pyenv][:version]}; pyenv rehash"
-# end
+execute "set global python 3.5.2" do
+  command "#{pyenv_init} pyenv global #{node[:pyenv][:version]}; pyenv rehash"
+end
 
-# execute "pip install ansible" do
-#   command "#{pyenv_init} pip install ansible"
-#   not_if "#{pyenv_init} pip freeze | grep ansible"
-# end
+execute "pip install ansible" do
+  command "#{pyenv_init} pip install ansible"
+  not_if "#{pyenv_init} pip freeze | grep ansible"
+end
