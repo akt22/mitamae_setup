a024711a29c4b1dd0b62e0f229767e7caef80b8e
Ryo Akita <akt.t.ksh.thy@gmail.com>
2017-03-31 02:12:18 +0900

    escape message

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 bash bin/setup_mitamae
 bin/mitamae local -y node.yml lib/recipe.rb
 bash $HOME/dotfiles/setup.sh
-echo "'chsh -s /bin/zsh' or 'exec $SHELL'"
+echo "'chsh -s /bin/zsh' or 'exec \$SHELL'"
 
 # exec $SHELL
